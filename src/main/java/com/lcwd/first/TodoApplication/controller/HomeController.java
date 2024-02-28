0cfb7a6f01d21883f69c29ba7eff511ade25d97f
kedarj24 <mayback@2024>
2024-02-28 20:04:29 +0530

    first commit

---
+++ src/main/java/com/lcwd/first/TodoApplication/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.lcwd.first.TodoApplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+@RestController
+public class HomeController {
+    @RequestMapping("/todos")
+    public List<String> justtest(){
+        return Arrays.asList("Learn Java","Learn Spring","Develop Projects"
+                ,"Learn Junit","Learn Mockito");
+    }
+}
+
